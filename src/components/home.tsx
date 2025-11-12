04c0bc0888d6a44f790e21aba164eed6f6c79a09
preethamgshiva <preethamgshiva2004@gmail.com>
2025-11-12 16:25:05 +0530

    Add new model and animations

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,13 +4,14 @@
 import ProductImage from "../assets/home_page.png";
 import SecondSection from "./secondSection";
 import ThirdSection from "./thirdSection";
+import TextPressure from "./TextPressure";
 
 const LandingSection = () => {
   return (
     <div className="min-h-screen bg-white text-black font-sans relative sticky top-0 z-10 overflow-hidden">
       {/* Header */}
       <header className="absolute top-0 left-0 right-0 z-10 flex justify-between items-center p-6 lg:p-10">
-        <div className="text-xl font-bold uppercase tracking-widest">
+        <div className="text-3xl font-bold uppercase tracking-widest">
           Eternal
         </div>
       </header>
@@ -19,12 +20,23 @@ const LandingSection = () => {
       <div className="flex flex-col md:flex-row min-h-screen">
         {/* Left Section (Text) */}
         <div className="flex-1 flex flex-col justify-center p-6 pt-24 md:p-10 lg:p-20 md:w-1.2">
-          <h1 className="text-6xl lg:text-8xl xl:text-[10rem] font-extrabold leading-none mb-6">
-            feel <br /> better
-          </h1>
+          <div style={{ position: "relative", height: "200px" }}>
+            <TextPressure
+              text="feel better"
+              flex={true}
+              alpha={false}
+              stroke={false}
+              width={true}
+              weight={true}
+              italic={true}
+              textColor="#000000"
+              strokeColor="#ff0000"
+              minFontSize={40}
+            />
+          </div>
           <p className="text-lg text-gray-700 max-w-md mb-10">
-            we have used plants for thousands of years in our search for better
-            wellness—Eternal is charting the future of plant-based remedies
+            We have used scents for thousands of years to express
+            ourselves—Eternal is redefining the future of fragrance.
           </p>
           <button className="bg-black text-white px-8 py-4 text-lg font-medium tracking-wide w-fit hover:bg-gray-800 transition-colors">
             shop Eternal
